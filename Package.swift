870fe496b86fa7c150fb57f6e0c9c9ea85dcfca7
John Haney <5025692+johnhaney@users.noreply.github.com>
2025-07-10 00:35:21 -0500

    Use ARUnderstanding 18.0.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
             targets: ["HandGesture"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/johnhaney/ARUnderstanding", branch: "main"),
+        .package(url: "https://github.com/johnhaney/ARUnderstanding", from: "18.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
